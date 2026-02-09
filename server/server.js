7b198b074e756c4d3d6ab55375f0bdd27171adb3
MOHITKOURAV01 <mohitdas852@gmail.com>
2026-02-09 16:29:54 +0530

    Connect MongoDB to application

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+// Connect to Database
+connectDB();
 
 // Middleware
 app.use(express.json());
@@ -9,7 +15,7 @@ app.get('/', (req, res) => {
     res.send('BookSmart API is running');
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
