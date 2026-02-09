b7d2de90dec86a01e647a0e11c6e873cf5b07765
MOHITKOURAV01 <mohitdas852@gmail.com>
2026-02-09 16:30:55 +0530

    Integrate book routes with server

--- server/server.js
+++ server/server.js
@@ -15,6 +15,9 @@ app.get('/', (req, res) => {
     res.send('BookSmart API is running');
 });
 
+// Routes
+app.use('/api/books', require('./routes/bookRoutes'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
