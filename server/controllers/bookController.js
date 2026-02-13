da7309306cddc469edaaf59412aecf2ebe22d1f0
MOHITKOURAV01 <mohitdas852@gmail.com>
2026-02-13 15:56:52 +0530

    Add get single book API

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -35,7 +35,24 @@ const createBook = async (req, res) => {
     }
 };
 
+// @desc    Get single book
+// @route   GET /api/books/:id
+const getBookById = async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        res.json(book);
+    } catch (error) {
+        res.status(500).json({ message: "Invalid Book ID" });
+    }
+};
+
 module.exports = {
     getBooks,
     createBook,
+    getBookById,
 };
--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getBooks, createBook } = require('../controllers/bookController');
+const { getBooks, createBook, getBookById } = require('../controllers/bookController');
 
 // @route   GET /api/books
 router.get('/', getBooks);
 
 // @route   POST /api/books
 router.post('/', createBook);
+
+// @route   GET /api/books/:id
+router.get('/:id', getBookById);
 
 module.exports = router;
