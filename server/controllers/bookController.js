7ed651a1f4a2f96bde7b520d9a82debb7cf3219b
MOHITKOURAV01 <mohitdas852@gmail.com>
2026-02-13 15:55:54 +0530

    Add create book API

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,5 +1,7 @@
 // @desc    Get all books
 // @route   GET /api/books
+const Book = require('../models/Book');
+
 const getBooks = async (req, res) => {
     try {
         res.json({ message: "Books API working" });
@@ -8,6 +10,32 @@ const getBooks = async (req, res) => {
     }
 };
 
+// @desc    Add a new book
+// @route   POST /api/books
+const createBook = async (req, res) => {
+    try {
+        const { title, author, language, description, buyLinks } = req.body;
+
+        if (!title || !author) {
+            return res.status(400).json({ message: "Title and Author are required" });
+        }
+
+        const book = new Book({
+            title,
+            author,
+            language,
+            description,
+            buyLinks
+        });
+
+        const createdBook = await book.save();
+        res.status(201).json(createdBook);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 module.exports = {
     getBooks,
+    createBook,
 };
--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getBooks } = require('../controllers/bookController');
+const { getBooks, createBook } = require('../controllers/bookController');
 
 // @route   GET /api/books
 router.get('/', getBooks);
+
+// @route   POST /api/books
+router.post('/', createBook);
 
 module.exports = router;
