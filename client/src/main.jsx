b34af3b8c249f9b3ccd2dd1ce4ad585333d5e630
MOHITKOURAV01 <mohitdas852@gmail.com>
2026-04-30 00:51:27 +0530

    Fix React crash due to missing Google Client ID

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
 
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'dummy_client_id_for_testing';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
